0a34dac4a3e01c94f6a8ea7286def195245a2172
Divya <divyabhookya@yahoo.com>
2018-07-17 23:00:51 -0500

    compliments

---
+++ compliments.js
@@ -0,0 +1,20 @@
+var Compliments = new Array ();[
+'You are an awesome friend'
+'You are a gift to those around you.'
+'You are a smart cookie.'
+'You are awesome!'
+'I like your style.''
+'You have the best laugh.'
+'I appreciate you.'
+'You are the most perfect you there is.'
+'You are enough.'
+'You are strong.'
+'Your perspective is refreshing.'
+'I'm grateful to know you.'
+'You light up the room.'
+'You deserve a hug right now.'
+'You should be proud of yourself.'
+
+]
+
+var i = Math.floor(Compliments.length * Math.random());
--- functions.js
+++ functions.js
@@ -1,6 +1,29 @@
 
 
 
-function surprise() {
+function SocialMedia() {
 alert("Follow Me On Instagram @_divb")
+}
+
+function surprise() {
+  var Compliments = [
+  'You are an awesome friend',
+  'You are a gift to those around you.',
+  'You are a smart cookie.',
+  'You are awesome!',
+  'I like your style.',
+  'You have the best laugh.',
+  'I appreciate you.',
+  'You are the most perfect you there is.',
+  'You are enough.',
+  'You are strong.',
+  'Your perspective is refreshing.',
+  'I\'m grateful to know you.',
+  'You light up the room.',
+  'You deserve a hug right now.',
+  'You should be proud of yourself.'
+];
+
+  var index = Math.floor(Math.random() * Compliments.length);
+  alert(Compliments[index]);
 }
